428d5fb15e991e2066d627cee37d31ff12698c9d
naospennikov <vislouhi@gmail.com>
2024-07-26 01:43:20 +0500

    script links update

--- jslib/flx_render.js
+++ jslib/flx_render.js
@@ -1,7 +1,132 @@
-// const nnWorkerUrl = "/js/service_worker.js"
-// const audioProcessorUrl = "/js/audio_processor.js"
-const nnWorkerUrl = "https://cdn.jsdelivr.net/gh/dmisol/flexatar-virtual-webcam@latest/jslib/service_worker.js"
-const audioProcessorUrl = "https://cdn.jsdelivr.net/gh/dmisol/flexatar-virtual-webcam@latest/jslib/audio_processor.js"
+const nnWorkerUrl = "/js/service_worker.js"
+const audioProcessorUrl = "https://flexatar-generic-shared-files.s3.amazonaws.com/js/audio_processor.js"
+// const nnWorkerUrl = "https://cdn.jsdelivr.net/gh/dmisol/flexatar-virtual-webcam@latest/jslib/service_worker.js"
+// const audioProcesso
+
+class SpeechNN{
+    constructor() {
+        this.wav2melModel = null;
+        this.mel2phonModel = null;
+        this.phon2avecModel = null;
+        this.isReady = false;
+        this.readyCallback = null;
+        this.loadNetworks();
+    }
+    async loadNetworks(){
+        
+        this.wav2melModel = await tf.loadLayersModel('https://raw.githubusercontent.com/dmisol/flexatar-virtual-webcam/main/raw/wav2mel/model.json');
+//        this.wav2melModel = await tf.loadLayersModel('/file/wav2mel/model.json');
+        this.mel2phonModel = await tf.loadLayersModel('https://raw.githubusercontent.com/dmisol/flexatar-virtual-webcam/main/raw/mel2phon/model.json');
+//        this.mel2phonModel = await tf.loadLayersModel('/file/mel2phon/model.json');
+        this.phon2avecModel = await tf.loadLayersModel('https://raw.githubusercontent.com/dmisol/flexatar-virtual-webcam/main/raw/phon2avec/model.json');
+//        this.phon2avecModel = await tf.loadLayersModel('/file/phon2avec/model.json');
+
+        const inputData = tf.tensor2d([new Float32Array(800)]);
+        const melPredicted = this.wav2melModel.predict(inputData);
+        const melTensors = [];
+        for (let i = 0; i < 20; i++) {
+            melTensors.push(melPredicted);
+        }
+        const melGroup = tf.concat(melTensors,1);
+        const phoneGroup = this.mel2phonModel.predict(melGroup);
+        const start = [0, 0, 0, 0];
+        const end = [phoneGroup.shape[0], phoneGroup.shape[1],phoneGroup.shape[2]-1,phoneGroup.shape[3]];
+        const phoneGroup1 = tf.slice(phoneGroup,start,end);
+        const avec = this.phon2avecModel.predict(phoneGroup1);
+
+        this.isReady = true;
+        if (this.readyCallback) {
+            this.readyCallback();
+        }
+    }
+
+    async awaitResources() {
+        if (this.isReady) {
+          return Promise.resolve();
+        }
+
+        return new Promise((resolve) => {
+          this.readyCallback = resolve;
+        });
+    }
+}
+
+
+class AnimCalc{
+    constructor(speechNetworks) {
+        this.speechNetworks = speechNetworks;
+        this.melTensors = [];
+        this.zeroAnimState = new Float32Array(5);
+        this.zeroAnimState[0] = 0.5;
+        this.zeroAnimState[1] = 0.48;
+        this.zeroAnimState[2] = 0.52;
+        this.zeroAnimState[3] = 0.43;
+        this.zeroAnimState[4] = 0.46;
+
+    }
+    getAnim(aBuffer){
+        const result = this.makeMel(aBuffer);
+        if (result) {
+            return result;
+        }else{
+            return [0.0,0.0,0.0,0.0,0.0];
+        }
+    }
+    makeMel(aBuffer){
+        if (!this.speechNetworks.isReady) {return}
+//        tf.engine().startScope()
+        const inputData = tf.tensor2d([aBuffer]);
+        const mel = this.speechNetworks.wav2melModel.predict(inputData);
+
+
+        this.melTensors.push(mel)
+
+        if (this.melTensors.length == 20){
+
+            const melGroup = tf.concat(this.melTensors,1);
+
+            const phoneGroup = this.speechNetworks.mel2phonModel.predict(melGroup);
+            const start = [0, 0, 0, 0];
+            const end = [phoneGroup.shape[0], phoneGroup.shape[1],phoneGroup.shape[2]-1,phoneGroup.shape[3]];
+            const phoneGroup1 = tf.slice(phoneGroup,start,end);
+            const avec = this.speechNetworks.phon2avecModel.predict(phoneGroup1);
+            const avecSlice = tf.slice(avec,[0,10,0,0],[1,1,5,1]);
+            const avecArrayTF = avecSlice.arraySync()
+
+
+            const avecArray = new Float32Array(5);
+            for (let i = 0; i < 5; i++) {
+                avecArray[i] = avecArrayTF[0][0][i][0];
+            }
+//            this.avecToBshpCoef(avecArray,rEngine,onAnimate);
+
+            //Dispose tensors
+                this.melTensors[0].dispose();
+                inputData.dispose();
+                melGroup.dispose();
+                phoneGroup.dispose();
+                phoneGroup1.dispose();
+                avec.dispose()
+                avecSlice.dispose();
+            //-----
+            this.melTensors.shift()
+            return this.avecToBshpCoef(avecArray)
+        }
+
+
+    }
+    avecToBshpCoef(nnAvec){
+        const result = new Float32Array(5);
+        const amp = -7;
+         for (let i = 0; i < 5; i++) {
+                result[i] =amp*(nnAvec[i]-this.zeroAnimState[i]);
+         }
+         return result;
+//         onAnimate(result,rEngine);
+
+    }
+}
+
 
 
 var v2 = {
@@ -1047,7 +1172,24 @@ class FlexatarUnit{
         this.mouthPackage = null;
         this.facePackages = {};
 
-        const uint8Array = new Uint8Array(arrayBuffer);
+        let uint8Array = new Uint8Array(arrayBuffer);
+        // console.log("base64 ",Flexatar.base64)
+        if (true){
+            var enc = new TextDecoder("utf-8");
+            var arr = uint8Array
+            uint8Array = atob(enc.decode(arr))
+
+            let len = uint8Array.length;
+            let bytes = new Uint8Array(len);
+            
+            const now = window.performance.now()
+            for (let i = 0; i < len; i++) {
+                bytes[i] = uint8Array.charCodeAt(i);
+            }
+            // console.log("time ",now - window.performance.now())
+            uint8Array = bytes
+        }
+        // console.log(uint8Array);
         var blocks = unpackToBlocks(uint8Array);
         this.mouthPackage  = new FlexatarMouthUnit(flexatarCommon)
         if (this.checkIfNeedInsertMouth(blocks)){
@@ -2659,7 +2801,7 @@ function fetchArrayBuffer(url){
 
 class FlexatarClient{
     #token = null
-    static route = "/"
+    static route = "https://86s46xzcnk.execute-api.us-east-1.amazonaws.com/default/ftar"
     constructor(token) {
        this.#token = token
       
@@ -2668,7 +2810,7 @@ class FlexatarClient{
     getFlexatar(flexatarLink){
         
         const data = {
-            token: this.#token,
+            // token: this.#token,
             ftar: flexatarLink
         };
         
@@ -2709,6 +2851,7 @@ function getAllSupportedMimeTypes(...mediaTypes) {
   }
 
 class Flexatar{
+    base64 = true
     #signalFlexatarReady = null;
     #flexatarReady = new Promise((resolve,reject)=>{
         this.#signalFlexatarReady = resolve;
@@ -2719,15 +2862,17 @@ class Flexatar{
         const options = {
             method: 'POST', // Use 'POST', 'PUT', or 'PATCH' instead of 'GET'
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Accept':"application/octet-stream"
             },
             body: JSON.stringify(flexatarLink) // Convert the data object to a JSON string
         };
-            fetch(FlexatarClient.route+"ftar", options)
+            fetch(FlexatarClient.route, options)
                 .then(response => {
                     return response.arrayBuffer();
                     
                 }).then(flexatarPackage =>{
+                    // console.log(flexatarPackage)
                     Flexatar.#staticPackageReady.then(()=>{
                         const flexatarUnit = new FlexatarUnit(flexatarPackage,Flexatar.#staticPackage);
                         flexatarUnit.awaitResources().then(()=>{
@@ -2735,8 +2880,6 @@ class Flexatar{
                         })
                     });
                 })
-            
-       
     }
 
     static #staticPackage = null
@@ -3198,20 +3341,28 @@ class FlexatarAnimator {
 }
 
 
-
+const noWorklet = true
 
 class SpeechAnimator{
     active = false
     
     constructor(){
+        if (noWorklet)
+            this.animCalc = new AnimCalc(new SpeechNN());
+
         this.onFrame = null
-        const myWorker = new Worker(nnWorkerUrl);
-        myWorker.onmessage = (e) => {
-            
-            if (this.onFrame) this.onFrame(e.data.anim)
-        };
+        let myWorker
+        if (!noWorklet){
+            myWorker = new Worker(nnWorkerUrl);
+            myWorker.onmessage = (e) => {
+                
+                if (this.onFrame) this.onFrame(e.data.anim)
+            };
+        }
 
         this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
+        
+
         this.processorNode = Promise.all([
             this.audioContext.audioWorklet.addModule("https://cdn.jsdelivr.net/gh/aolsenjazz/libsamplerate-js/dist/libsamplerate.worklet.js"),
             this.audioContext.audioWorklet.addModule(audioProcessorUrl)
@@ -3222,7 +3373,12 @@ class SpeechAnimator{
                 // console.log("data",event.data)
                 const data = event.data  
                 if (data && this.active && data instanceof Float32Array) {
-                    myWorker.postMessage(data);
+                    if (noWorklet){
+                        if (this.onFrame) this.onFrame(this.animCalc.getAnim(event.data))
+                    }else{
+                        myWorker.postMessage(data);
+                    }
+                    
                 }else{
                     this.onFrame([0.0,0.0,0.0,0.0,0.0])
                 }
